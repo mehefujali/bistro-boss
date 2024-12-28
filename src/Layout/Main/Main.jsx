f219cd9d13cc19d8edb307e45b7f7474157d7ac8
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-28 22:36:52 +0530

    routes setup complite

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+      return (
+            <div>
+                  <div></div>
+                  <div>
+                        <Outlet></Outlet>
+                  </div>
+                  <div>
+                        
+                  </div>
+                  
+            </div>
+      );
+};
+
+export default Main;
