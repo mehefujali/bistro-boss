6dd7c261d7935f19853fa33d7f311f324ad8bd45
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-30 17:16:24 +0530

    menu card and ourmenu responsive

--- src/components/BistroBossAbout/BistroBossAbout.jsx
+++ src/components/BistroBossAbout/BistroBossAbout.jsx
@@ -4,9 +4,9 @@ const BistroBossAbout = () => {
       return (
             <div>
                   <div  className=" container mx-auto flex justify-center items-center my-14 p-8 md:p-10  lg:p-28 w-full  bg-cover" id='bistro-boss-about-bg'>
-                        <div className='  bg-white text-center bg-opacity-80  backdrop-blur-lg    w-full p-16 '>
+                        <div className='  bg-white text-center bg-opacity-80  backdrop-blur-lg    w-full md:p-16 p-5 '>
                               <div>
-                                    <h1 className=' text-5xl mb-6 text-yellow-600 '>Bistro Boss</h1>
+                                    <h1 className=' text-3xl md:text-5xl mb-6 text-yellow-600 '>Bistro Boss</h1>
                                     <p>Globally customize synergistic strategic theme areas before user friendly collaboration and idea-sharing. Completely envisioneer enabled products before unique services. Distinctively drive 2.0 paradigms and user-centric intellectual capital. Assertively predominate excellent expertise before standardized manufactured products. Professionally expedite high standards in supply chains without integrated supply.</p>
                               </div>
                         </div>
--- src/components/MenuCard/MenuCard.jsx
+++ src/components/MenuCard/MenuCard.jsx
@@ -5,10 +5,10 @@ const MenuCard = ({ menu }) => {
   return (
     <div>
       <div className=" flex items-center gap-6">
-        <img className=" w-28 h-28 object-cover rounded-full rounded-tl-none" src={image} alt="" />
+        <img className=" w-24 h-24 md:w-28 md:h-28 object-cover rounded-full rounded-tl-none" src={image} alt="" />
         <div>
           <div className=" flex items-center justify-between">
-            <h3 className=" text-lg md:text-xl xl:text-2xl ">{name}--------</h3> <p className=" text-yellow-600 ">${price}</p>
+            <h3 className=" text-sm md:text-xl xl:text-2xl ">{name}----</h3> <p className=" text-yellow-600 ">${price}</p>
           </div>
           <p className=" text-gray-400">{recipe}</p>
         </div>
