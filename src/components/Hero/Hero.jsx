a996db2c09581fa5003d6b1fb984851a7fdd9268
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-30 10:37:58 +0530

    swiper js complited

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,74 +6,44 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./swiper.css";
-// import required modules
+
+// Import required modules
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 export default function App() {
   return (
-    <>
-      <div className="">
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Pagination, Navigation, Autoplay]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            {" "}
-            <img
-              className="  "
-              src="https://i.imgur.com/ze9GaHS.jpg"
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img
-              className="  "
-              src="https://i.imgur.com/WQO6Jh9.png"
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img
-              className="  "
-              src="https://i.imgur.com/EPD97Vc.jpg"
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img
-              className="  "
-              src="https://i.imgur.com/Ab9ssc7.png"
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img
-              className="  "
-              src="https://i.imgur.com/xa6ZBqA.jpg"
-              alt=""
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <img
-              className="  "
-              src="https://i.imgur.com/T6rhO0c.jpg"
-              alt=""
-            />
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </>
+    <div className="swiper-container">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://i.imgur.com/ze9GaHS.jpg" alt="Slide 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.imgur.com/WQO6Jh9.png" alt="Slide 2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.imgur.com/EPD97Vc.jpg" alt="Slide 3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.imgur.com/Ab9ssc7.png" alt="Slide 4" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.imgur.com/xa6ZBqA.jpg" alt="Slide 5" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
