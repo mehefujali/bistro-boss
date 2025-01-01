885d8c4f6bebece9c34194a6f34854a59dcc7c46
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-01 13:06:35 +0530

    login page crated

--- src/auth/Login/Login.jsx
+++ src/auth/Login/Login.jsx
@@ -1,10 +1,66 @@
-
 const Login = () => {
-      return (
-            <div>
-                  
-            </div>
-      );
+  return (
+    <div 
+    className=" h-[100vh] w-[100vw]  flex items-center justify-center bg-cover"
+    style={{backgroundImage:'url(https://i.imgur.com/54Ky7iG.png)'}}
+    
+    >
+      <div className=" w-fit h-fit flex items-center gap-4 border shadow-lg p-8 lg:p-20 2xl:p-28 rounded shadow-gray-500">
+        <div>
+          <img src="https://i.imgur.com/dKZiHzX.png" alt="" />
+        </div>
+        <div className=" w-6/12">
+          <form className=" flex w-full flex-col gap-1 items-center">
+            <label htmlFor="email" className=" w-full flex flex-col gap-1 items-start">
+              <span>Email : </span>
+              <input
+                type="text"
+                placeholder="Enter your email"
+                name=""
+                id="email"
+                className=" w-full input focus:outline-none border-gray-400 rounded"
+              />
+            </label>
+            <label
+              htmlFor="password"
+              className=" w-full flex flex-col gap-1 items-start"
+            >
+              <span>Password : </span>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                name=""
+                id="password"
+                className=" w-full input focus:outline-none border-gray-400 rounded"
+              />
+            </label>
+            <label 
+            className=" w-full flex flex-col gap-1 my-2 items-start"
+            htmlFor="">
+            <div 
+              readOnly
+              type="text"
+              
+              name=""
+              id=""
+              className=" select-none w-full input  flex items-center focus:outline-none border-gray-400 rounded"
+            >afddf </div>
+            <button className=" text-blue-500">Reload captcha</button>
+            </label>
+            <input
+              
+              type="text"
+               placeholder="Type here"
+              name=""
+              id="password"
+              className=" w-full input focus:outline-none border-gray-400 rounded"
+            />
+            <button className="btn w-full bg-[#D1A054B3] text-white rounded">Sign in</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
