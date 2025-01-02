673121c78a7790d95658550e386d09284c3f128b
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-02 11:15:36 +0530

    google login  created in register page

--- src/auth/Register/Register.jsx
+++ src/auth/Register/Register.jsx
@@ -1,8 +1,18 @@
+import { useContext } from "react";
 import { FaFacebookF, FaGoogle } from "react-icons/fa";
 import { IoLogoGithub } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Register = () => {
+  const { googleSignIn } = useContext(AuthContext);
+  const handleGooogleSignIn = () => {
+    googleSignIn()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(() => {});
+  };
   return (
     <div>
       <div
@@ -16,6 +26,19 @@ const Register = () => {
           <div className=" md:w-6/12">
             <form className=" flex w-full flex-col gap-1 items-center">
               <label
+                htmlFor="name"
+                className=" w-full flex flex-col gap-1 items-start"
+              >
+                <span>Name : </span>
+                <input
+                  type="text"
+                  placeholder="Enter your Name"
+                  name="name"
+                  id="name"
+                  className=" w-full input focus:outline-none border-gray-400 rounded"
+                />
+              </label>
+              <label
                 htmlFor="email"
                 className=" w-full flex flex-col gap-1 items-start"
               >
@@ -23,7 +46,7 @@ const Register = () => {
                 <input
                   type="text"
                   placeholder="Enter your email"
-                  name=""
+                  name="email"
                   id="email"
                   className=" w-full input focus:outline-none border-gray-400 rounded"
                 />
@@ -36,18 +59,18 @@ const Register = () => {
                 <input
                   type="password"
                   placeholder="Enter your password"
-                  name=""
+                  name="password"
                   id="password"
                   className=" w-full input focus:outline-none  border-gray-400 rounded"
                 />
               </label>
-              <button className="btn w-full disabled:cursor-not-allowed bg-[#D1A054B3] text-white rounded">
+              <button className="btn mt-4 w-full disabled:cursor-not-allowed bg-[#D1A054B3] text-white rounded">
                 Sign Up
               </button>
             </form>
             <div className=" mt-2 text-center">
               <span className=" text-[#D1A054]">
-              Already registered? 
+                Already registered?
                 <Link className=" font-semibold" to="/login">
                   {" "}
                   Go to log in
@@ -55,15 +78,15 @@ const Register = () => {
               </span>
               <p>Or sign up with</p>
               <div className=" flex w-fit gap-2 mx-auto mt-3">
-                <div className=" text-lg p-2 rounded-full border border-black w-fit">
+                <div className=" cursor-pointer text-lg p-2 rounded-full border border-black w-fit">
                   {" "}
                   <FaFacebookF />
                 </div>
-                <div className=" text-lg p-2 rounded-full border border-black w-fit">
+                <div onClick={handleGooogleSignIn} className=" cursor-pointer text-lg p-2 rounded-full border border-black w-fit">
                   {" "}
                   <FaGoogle />
                 </div>
-                <div className=" text-lg p-2 rounded-full border border-black w-fit">
+                <div className=" cursor-pointer text-lg p-2 rounded-full border border-black w-fit">
                   {" "}
                   <IoLogoGithub />
                 </div>
