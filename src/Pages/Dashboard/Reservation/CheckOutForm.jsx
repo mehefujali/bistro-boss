50813d2a790bce8cbdd3f9f7d45294391e632428
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-09 18:43:25 +0530

    payment system impliment with stripe

--- src/Pages/Dashboard/Reservation/CheckOutForm.jsx
+++ src/Pages/Dashboard/Reservation/CheckOutForm.jsx
@@ -1,16 +1,68 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useCart from "../../../Hooks/useCart";
+import { useState } from "react";
+import useAuth from "../../../Hooks/useAuth";
 
 const CheckOutForm = () => {
   const stripe = useStripe();
-  const element = useElements()
+  const element = useElements();
+  const [clientSecret, setClientSecret] = useState();
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const [cart] = useCart();
+  const price = cart?.reduce((total, item) => total + item.price, 0);
+
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", { price }).then((res) => {
+      setClientSecret(res.data.clientSecret);
+    });
+  }, [axiosSecure, price]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if(!stripe || !element){
-      return
+    if (!stripe || !element) {
+      return;
     }
-    const card = element.getElement(CardElement)
-    if(card == null) {
-      return
+    const card = element.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("payment error", error);
+      toast.error(error.message);
+    } else {
+      console.log("payment methode", paymentMethod);
+     
+    }
+
+    //     conform payments
+    const { paymentIntent, error: conError } = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymus",
+            name: user?.displayName || "anonymus",
+          },
+        },
+      }
+    );
+    if (conError) {
+      console.log("payment error ", conError);
+    } else {
+      console.log("payment intant ", paymentIntent);
+      if(paymentIntent.status === "succeeded"){
+            toast.success("Payment success");
+      }
     }
   };
   return (
@@ -31,7 +83,11 @@ const CheckOutForm = () => {
           },
         }}
       />
-      <button type="submit" className=" mt-10 bg-green-500 hover:bg-green-600 btn rounded-md w-full" disabled={!stripe}>
+      <button
+        type="submit"
+        className=" mt-10 bg-green-500 hover:bg-green-600 btn rounded-md w-full"
+        disabled={!stripe || !clientSecret}
+      >
         Pay
       </button>
     </form>
