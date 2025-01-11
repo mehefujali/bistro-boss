c905dfceffb64f478b4408ca0c1c806fc7e8a031
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-11 10:16:34 +0530

    chef reacommends food changed

--- src/components/ChefRecommends/ChefRecommends.jsx
+++ src/components/ChefRecommends/ChefRecommends.jsx
@@ -1,23 +1,18 @@
-import { useEffect, useState } from "react";
+
 import SectionHeading from "../SectionHeading/SectionHeading";
-import axios from "axios";
+
 import FoodCard from "../FoodCard/FoodCard";
+import useMenu from './../../Hooks/useMenu';
 
 const ChefRecommends = () => {
-      const [recommends,setRecommends] = useState([])
-      useEffect(()=>{
-        axios.get('https://raw.githubusercontent.com/ProgrammingHero1/bistro-boss-restaurant-resources/refs/heads/main/menu.json')
-        .then(res => {
-            const recommendsData = res.data.filter(food => food.category === 'popular')
-            setRecommends(recommendsData)
-        })
-      },[])
+      const [recommends] = useMenu()
+      
   return (
     <div className=" my-14">
       <SectionHeading heading={'CHEF RECOMMENDS'} subHeading={'Should Try'}/>
       <div className=" container w-11/12 xl:w-full mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 xl:gap-8">
                {
-                  recommends.map(recommend => <FoodCard key={recommend._id} recommend={recommend}></FoodCard>)
+                  recommends.slice(0,4).map(recommend => <FoodCard key={recommend._id} recommend={recommend}></FoodCard>)
                }
       </div>
     </div>
